1388cb7fbd472437bbd78f1aee4bbaf0450a62ce
oyemoney <ltakintola@gmail.com>
2022-12-08 13:46:54 -0800

    c.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+/* A program that prists some text */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
