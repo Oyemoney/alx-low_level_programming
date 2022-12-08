269eec429104c74f5b2ae6965fe03395e6cfc3a2
oyemoney <ltakintola@gmail.com>
2022-12-08 13:26:23 -0800

    c.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 /* A program that ptints out a text */
-
 #include <stdio.h>
 /**
  * main - Entry point
@@ -9,6 +8,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-
 	return (0);
 }
