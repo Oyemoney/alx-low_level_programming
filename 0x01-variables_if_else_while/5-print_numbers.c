3b01d1158b3a876c39c3bf2248ff5da69a226deb
oyemoney <ltakintola@gmail.com>
2022-12-09 15:14:34 -0800

    c.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar(n % 10 + '0');
+		i++;
+	}
+	putchar('\n');
+}
