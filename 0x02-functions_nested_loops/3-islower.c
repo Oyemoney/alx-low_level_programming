14163f2657bb0ed829db72af70ea8dadfe3452f6
oyemoney <ltakintola@gmail.com>
2022-12-13 15:20:53 -0800

    c.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _islower - A function that prints lowercase ccharactesr
+ *
+ * Return: returns an integer
+ */
+int _islower(int c)
+{
+	char i;
+
+	if (i = 'a' && i <= 'z')
+	{
+		c = 1;
+		return (c);
+	}
+	else 
+	{
+		c = 0;
+		return (c);
+	}
+}
