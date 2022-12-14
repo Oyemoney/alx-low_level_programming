93ca36e2bb9d5c0da7422c7cfa11fe5e388e8699
oyemoney <ltakintola@gmail.com>
2022-12-14 12:43:03 -0800

    c.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,12 +2,12 @@
 
 /**
  * _abs - A function that print the absolute value of an integer
- * @n: integer to be tested 
+ * @c: integer to be tested
  * Return: returns an integer
  */
 int _abs(int c)
 {
-	if(c < 0)
+	if (c < 0)
 	{
 		c = c * (-1);
 		return (c);
