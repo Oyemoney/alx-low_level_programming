1c6ea777beb91d3552270e4ed57aea2d783e1db2
oyemoney <ltakintola@gmail.com>
2022-12-15 12:56:02 -0800

    c.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * more_numbers - A function that checks uppercase characters
+ *
+ * Return: returns void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			_putchar(j % 10 +'0');
+			if (j >= 10)
+			{ 
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
+
