bd06d580fcd777e5acbab44ac5aa62b91aff1951
oyemoney <ltakintola@gmail.com>
2022-12-15 11:21:42 -0800

    c.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - adds two numbers together.
+ * @a: The first number to be added.
+ * @b: The second number to be added.
+ * Return: the sum of two numbers.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
