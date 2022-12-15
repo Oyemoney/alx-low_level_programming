3cf0dcffb2837a17304c414ccece3b71997818ee
oyemoney <ltakintola@gmail.com>
2022-12-15 13:42:31 -0800

    c.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,10 +7,10 @@
  */
 void print_lines(int n)
 {
-	int i;
-
 	if (n > 0)
 	{
+		int i;
+
 		for (i = 0; i <= n; i++)
 		{
 			_putchar('-');
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,4 +1,5 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 int _isupper(int c);
@@ -8,3 +9,5 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+
+#endif
