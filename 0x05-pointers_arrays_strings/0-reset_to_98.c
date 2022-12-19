d792ff67f03393bf75b42d0e965641c30318cc8a
oyemoney <ltakintola@gmail.com>
2022-12-19 12:26:04 -0800

    c.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - A function that changes the value of a variable
+ * @*n: pointer to be replaced
+ * Return: returns void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+1. 0-reset_to_98.c: A function that changes the valur of a variable.
