831f646c10492469d335791c32856ae7b39d02f1
oyemoney <ltakintola@gmail.com>
2022-12-19 13:34:09 -0800

    c.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - A function that changes the value of a variable
+ * @a: pointer to be replaced
+ * @b: pointer to be replaced
+ * Return: returns void
+ */
+
+void swap_int(int *n)
+{
+	int n;
+
+	n = *a;
+	*a = *b;
+	*b = n;
+}
