9e4b2c5ec4f429f8e1ca642f490aae74837e1281
oyemoney <ltakintola@gmail.com>
2023-01-02 12:07:02 -0800

    c.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,12 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _memset - A function that changes values stored in a memory block.
  * @s: pointer to the starting memory area
  * @b: variable that stores the value that replaces the previouds value
  * @n: nymber of bytes to be replaced
+ * Return: returns  the pointer s
  */
 
 char *_memset(char *s, char b, unsigned int n)
