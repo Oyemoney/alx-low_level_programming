c0bad4157bfe7832773fdd97cc4204c372b8d1d2
oyemoney <ltakintola@gmail.com>
2023-01-07 12:49:01 -0800

    cing the world tru recursive lens

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - A function that returns the number of character in a string
+ * _strlen_recursion - A function that returns the number of character 
+ * in a string
  * @s: strings of characters to be printed
  * Return: returns no. of characters
  */
