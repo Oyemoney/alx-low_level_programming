897c5c0a06923bb8b413e233a4634e9ba2e0a944
oyemoney <ltakintola@gmail.com>
2023-01-07 13:10:53 -0800

    cing the world tru recursive lens

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * factorial - A function calculates factorial
+ * @n: strings of characters to be printed
+ * Return: returns solution to the factorial
+ */
+
+int factorial(char *s)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * (factorial(n - 1)));
+	}
+}
