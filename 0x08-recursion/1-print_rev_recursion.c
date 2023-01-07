21477ff8c5e013b23478df68bca132d4897dc681
oyemoney <ltakintola@gmail.com>
2023-01-07 12:28:11 -0800

    cing the world tru recursive lens

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - A function that reverses a string
+ * @s: string to be reversed
+ * Return: returns void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	else
+	{
+		_puts_recursion(s + 1);
+		_putchar(*s);
+	}
+}
