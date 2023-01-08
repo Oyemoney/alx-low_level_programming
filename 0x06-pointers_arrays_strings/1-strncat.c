c3c9b86e246be31c6753d473600bde7e0a3c34b9
oyemoney <ltakintola@gmail.com>
2023-01-08 10:13:50 -0800

    cing the world tru recursive lens

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * char *_strcat - A function that concatenates two strings
+ * @dest: string to be copied to
+ * @src: string to copy from
+ * @n: number of bytes to be copied
+ * Return: returns dest
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		/**
+		 * ntin in the loop
+		 */
+	}
+	for (j = 0; j <= n; j++)
+	{
+		dest[i] = src[j];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
