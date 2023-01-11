15e93004b3d83b368285ff3f1b2733f0dccdcaf2
oyemoney <ltakintola@gmail.com>
2023-01-10 19:50:10 -0800

    cing the world tru recursive lens

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * char *_strncpy - a function that copies the string pointed to by src
+ * @dest: copy to
+ * @src: copy from
+ * @n: number of bytes to copy
+ * Return: string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
